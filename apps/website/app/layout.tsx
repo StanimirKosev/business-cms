5bf550c91f8ede347a96f91a29baed6636aad785
StanimirKosev <stanimirkosev00@gmail.com>
2025-10-22 10:26:58 +0300

    Refactor Layout and Update Font Integration
    - Replaced Geist and Geist_Mono fonts with Manrope in the layout component for improved typography.
    - Updated global CSS to reflect the new font integration, ensuring consistent font usage across the application.

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import { Toaster } from "sonner";
 import { LanguageProvider } from "@/app/context/LanguageContext";
 
-// Fonts
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// Font
+const manrope = Manrope({
+  subsets: ["latin", "latin-ext"], // latin-ext for Bulgarian characters
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-manrope",
 });
 
 // SEO Metadata
@@ -32,9 +28,7 @@ export default function RootLayout({
   return (
     // Language for SEO
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${manrope.variable} antialiased`}>
         <LanguageProvider>
           <Header />
           {children}
